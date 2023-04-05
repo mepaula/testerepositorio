a38cb25bb40c36dd2935efc921da155bc58c76e7
mepaula <129890256+mepaula@users.noreply.github.com>
2023-04-05 12:08:32 -0300

    somar

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener( "click" ,showcolor );
 
 document.getElementById("botao").addEventListener("click" , exibenome)
 
+document.getElementById("btn-soma").addEventListener("click" , soma)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -13,9 +15,9 @@ function exibenome()
     document.getElementById( "mensagem" ).innerText = "Meu nome e: " + usuario;
     document.getElementById( "nome" ).value = "";
 }
-function number()
+function soma()
 {
-    let numero = document.getElementById( "num1" ).value;
-    document.getElementById( "resultado1" ).innerText = "soma" + numero;
-    document.getElementById( "num2" ).value = "";
+    let numero1 = document.getElementById( "num1" ).value;
+    let numero2 = document.getElementById( "num2" ).value;
+    document.getElementById( "resultado1" ).innerText = parseFloat(numero1) + parseFloat(numero2);
 }
