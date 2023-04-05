f548448753b4049745a37a4cf838a208c7d0580e
mepaula <129890256+mepaula@users.noreply.github.com>
2023-04-05 10:55:03 -0300

    alteraçao

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 document.addEventListener( "click" ,showcolor );
 
+document.getElementById("botao").addEventListener("click" , exibenome)
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
 }
 
+function exibenome()
+{
+    
+}
